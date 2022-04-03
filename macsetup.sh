e7e69752a8a1a41867df3c5bb5808ae0196e6b6d
Quinn Bellmore <93296277+quinnzbellz@users.noreply.github.com>
2022-04-03 10:20:32 -0400

    adding stuff

--- macsetup.sh
+++ macsetup.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 softwareupdate -i -a
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sleep 5
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/quinnbellmore/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 source ~/.zprofile
 sleep 5
 cat pkgs.txt | xargs brew install
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0 
+killAll Finder && killAll Dock
