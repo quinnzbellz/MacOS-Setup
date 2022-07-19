b175cc9ec64b79826f09b8dffe2052d505ad954c
Quinn Bellmore <93296277+quinnzbellz@users.noreply.github.com>
2022-07-19 08:42:30 -0400

    added left dock and updated brewfile

--- macsetup.sh
+++ macsetup.sh
@@ -11,4 +11,5 @@ defaults write com.apple.dock launchanim -bool false
 defaults write com.apple.dock expose-animation-duration -float 0.1
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0 
+defaults write com.apple.dock orientation left
 killAll Finder && killAll Dock
